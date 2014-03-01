28cbbd363109f4d5fb430dff68c95d91ed4d312b
Lech Rzedzicki <xchaotic@gmail.com>
2014-03-01 08:38:35 +0000

    generated first rectangle

--- InitColorPicker.js
+++ InitColorPicker.js
@@ -1,8 +1,17 @@
 function InitColorPicker ()
 {
  var canvas = document.createElement('canvas');
+ var ctx = canvas.getContext('2');
+ canvas.width = 256;
+ canvas.height = 256;
 
+document.body.appendChild(canvas);
+
+ctx.fillRect(0,0, 100, 100);
 
  for (var i = 0; i < 256; i++)
- { }   
+ { 
+ctx.fillStyle = 'rgb(i, 0, 255)';    
+ctx.fillRect(0,0, 100, 100);
+ }   
 }
