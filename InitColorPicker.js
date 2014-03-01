a7a96eaef2f0614431931acd61fa671e76f89a09
Lech Rzedzicki <xchaotic@gmail.com>
2014-03-01 08:46:44 +0000

    bug fixing already

--- InitColorPicker.js
+++ InitColorPicker.js
@@ -1,7 +1,7 @@
 function InitColorPicker ()
 {
  var canvas = document.createElement('canvas');
- var ctx = canvas.getContext('2');
+ var ctx = canvas.getContext('2d');
  canvas.width = 256;
  canvas.height = 256;
 
@@ -10,9 +10,10 @@ document.body.appendChild(canvas);
 ctx.fillRect(0,0, 255, 255);
 
  for (var x = 0; x < 256; x++)
- { 
-    for (var  y = 0; y < 256; y++)
-ctx.fillStyle = 'rgb(x, y, 255)';    
-ctx.fillRect(x,y, x+1, y+1);
+    { 
+        for (var  y = 0; y < 256; y++){
+            ctx.fillStyle = 'rgb(' + x + ',' + y + ', 255)';    
+            ctx.fillRect(x,y, 1, 1);
+    }
  }   
 }
